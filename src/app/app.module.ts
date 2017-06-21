8ff289d78afb5b6c94b1bf826446677eaf935c33
girish <girish.desai@nevisnetworks.com>
2017-06-21 16:47:30 +0530

    Modify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
-import { NewComponentComponent } from './new-component/new-component.component';
 import { NewBooksComponent } from './new-books/new-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    NewComponentComponent,
     NewBooksComponent
   ],
   imports: [
